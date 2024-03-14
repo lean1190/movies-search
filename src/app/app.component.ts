0ae1f8b0ae1c4759fda85db0bff8eba9be489a45
Lean <leian1306@gmail.com>
2024-03-14 12:02:22 +0100

    app: add example button with PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    BrowserModule,
-    BrowserAnimationsModule
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        ButtonModule
     ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'favorite-movies-updated';
 }
